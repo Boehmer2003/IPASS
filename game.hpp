1b406518b53c0e20491198de837057df5dfbeeb8
Benjamin Boehmer <benjaminboehmer@live.nl>
2022-06-27 11:22:25 +0200

    added license

--- game.hpp
+++ game.hpp
@@ -5,6 +5,9 @@
 //       Part of :   IPASS
 //
 //
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
 // # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 /// @file
